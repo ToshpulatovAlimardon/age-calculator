407f43bb246a29f6bb105d8c97bd43599a9a3428
ToshpulatovAlimardon <alimardontoshpulatov001@gmail.com>
2024-07-21 10:48:53 +0500

    added javasript

---
+++ script.js
@@ -0,0 +1,32 @@
+const btnEl = document.querySelector("#btn");
+const birthdayEl = document.querySelector("#birthday");
+const resultEl = document.querySelector("#result");
+
+function calculateAge() {
+  const birthdayValue = birthdayEl.value;
+  if (birthdayValue === "") {
+    alert("Please enter your birthday");
+  } else {
+    const age = getAge(birthdayValue);
+    resultEl.innerText = `Your age is ${age} ${age > 1 ? "years" : "year"} old`;
+  }
+}
+
+function getAge(birthdayValue) {
+  const currentDate = new Date();
+  const birthdayDate = new Date(birthdayValue);
+
+  let age = currentDate.getFullYear() - birthdayDate.getFullYear();
+  const month = currentDate.getMonth() - birthdayDate.getMonth();
+
+  if (
+    month < 0 ||
+    (month === 0 && currentDate.getDate() < birthdayDate.getDate())
+  ) {
+    age--;
+  }
+
+  return age;
+}
+
+btnEl.addEventListener("click", calculateAge);
